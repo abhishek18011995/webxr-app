449323969ed02ff6c38fbfe347a014b2d6e57f27
Kumar Abhishek <Z004FF8J@siemens.cloud>
2025-04-16 18:54:34 +0530

    added video and image in xrpanel

--- src/components/xr-overlay/panel/XrStep1.jsx
+++ src/components/xr-overlay/panel/XrStep1.jsx
@@ -1,6 +1,9 @@
-import React, { useRef, useEffect } from 'react';
-import { Text, RoundedBox } from '@react-three/drei';
+import React, { useRef, useEffect, useMemo } from 'react';
+import { Text, RoundedBox, Html } from '@react-three/drei';
 import { useNavigate } from 'react-router-dom';
+import { XRLayer } from '@react-three/xr';
+// import {HTMLImageElement} from 'three/examples/jsm/nodes/core/HTMLImageElement.js';
+
 
 const XrStep1 = () => {
     const navigate = useNavigate();
@@ -19,6 +22,60 @@ const XrStep1 = () => {
         }
     }, []);
 
+    const handlePointerDown = (e) => {
+        pointerDownRef.current = true;
+        offsetRef.current = {
+            x: meshRef.current.position.x - e.point.x,
+            y: meshRef.current.position.y - e.point.y,
+            z: meshRef.current.position.z - e.point.z,
+        };
+    }
+
+    const handlePointerMove = (e) => {
+        if (!pointerDownRef.current) {
+            return
+        }
+
+        e.stopPropagation();
+        meshRef.current.position.set(
+            e.point.x + offsetRef.current.x,
+            e.point.y + offsetRef.current.y,
+            meshRef.current.position.z // Keep Z constant
+        );
+        // console.log('onPointerMove', meshRef.current.position);
+    }
+
+    const handlePointerUp = () => {
+        offsetRef.current = null;
+        pointerDownRef.current = false;
+    }
+
+    const video = useMemo(() => {
+        const result = document.createElement('video')
+        result.src = './../vd1.mp4'
+        // console.log(result);
+        return result
+    }, [])
+
+    const renderHtml = useMemo(() => {
+        // const result = document.createElement('div');
+        // result.innerHTML = `Hello World!`;
+        // result.style.width = '100%';
+        // result.style.height = '100%';   
+        // result.style.backgroundColor = 'white';
+        // result.style.color = 'black';
+
+        const result = document.createElement('img');
+        // const result = new HTMLImageElement();
+        result.src = './../react.png';
+        result.alt = './../react.png';
+        result.width = 300;
+        result.height = 200;
+        console.log(result);
+        return result
+    }, [])
+
+
     return (
         <>
             <ambientLight intensity={1} />
@@ -27,36 +84,9 @@ const XrStep1 = () => {
                 <mesh
                     position={relativePosition}
                     ref={meshRef}
-                    onPointerDown={(e) => {
-                        pointerDownRef.current = true;
-                        // meshRef.current.position.copy(e.point)
-
-                        offsetRef.current = {
-                            x: meshRef.current.position.x - e.point.x,
-                            y: meshRef.current.position.y - e.point.y,
-                            z: meshRef.current.position.z - e.point.z,
-                        };
-
-                        console.log('onPointerDown', meshRef.current.position);
-                    }}
-                    onPointerMove={(e) => {
-                        if (!pointerDownRef.current) {
-                            return
-                        }
-
-                        e.stopPropagation();
-                        meshRef.current.position.set(
-                            e.point.x + offsetRef.current.x,
-                            e.point.y + offsetRef.current.y,
-                            meshRef.current.position.z // Keep Z constant
-                        );
-
-                        console.log('onPointerMove', meshRef.current.position);
-                    }}
-                    onPointerUp={() => {
-                        offsetRef.current = null;
-                        pointerDownRef.current = false;
-                    }}>
+                    onPointerDown={handlePointerDown}
+                    onPointerMove={handlePointerMove}
+                    onPointerUp={handlePointerUp}>
                     <RoundedBox args={[1, 0.7, 0.02]} radius={0.02}  >
                         <meshStandardMaterial color="#add8e6" />
                     </RoundedBox>
@@ -68,6 +98,7 @@ const XrStep1 = () => {
                         Step 1 Guide
                     </Text>
                 </mesh>
+
                 <mesh
                     position={[relativePosition[0], relativePosition[1] - 0.45, relativePosition[2] + 0.02]}>
                     <RoundedBox
@@ -85,6 +116,16 @@ const XrStep1 = () => {
                         Next
                     </Text>
                 </mesh>
+
+                <XRLayer position={[0, 1.5, -1]} onClick={() => video.play()} scale={0.5} src={video} />
+                <XRLayer position={[0, 2.5, -2]} scale={0.5} src={renderHtml}/>
+                {/* <XRLayer position={[0, 1.5, -1]} scale={0.5}>
+                    <mesh>
+                        <boxGeometry />
+                        <meshBasicMaterial color="red" />
+                    </mesh>
+                </XRLayer> */}
+
             </group>
         </>
     )
